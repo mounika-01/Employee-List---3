bd9486cac8ae2cc086e2ebe8bb6b066233f3a9a4
mounika-01 <67068850+mounika-01@users.noreply.github.com>
2024-06-29 11:17:48 +0530

    Update EmployeeJpaRepository.java

--- src/main/java/com/example/employee/repository/EmployeeJpaRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeJpaRepository.java
@@ -1,8 +1,10 @@
-package com.example.employee.respository;
+package com.example.employee.repository;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;;
+import org.springframework.stereotype.Repository;
 import com.example.employee.model.Employee;
+
 
 @Repository
 public interface EmployeeJpaRepository extends JpaRepository<Employee, Integer> {
